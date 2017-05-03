2d2c56999a8812a40f247b915aa84e83b4d90c80
Pablo Segura <pablos@thehangar.cr>
2017-05-03 14:46:47 -0600

    Mock API initial

---
+++ server-compiled.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _bodyParser = require('body-parser');
+
+var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
+var _jsonwebtoken = require('jsonwebtoken');
+
+var _jsonwebtoken2 = _interopRequireDefault(_jsonwebtoken);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var secret = "SOMECOOLSECRET"; /**
+                                * Created by pablos on 5/3/17.
+                                */
+
+
+var app = (0, _express2.default)();
+
+app.use(_bodyParser2.default.json());
+app.use(_bodyParser2.default.urlencoded({ extended: true }));
+
+app.listen(3000, function () {
+    console.log('listening on 3000');
+});
+
+app.post("/auth", function (req, res) {
+    var email = "email@email.com";
+    var pass = "pass";
+    var body = _jsonwebtoken2.default.verify(req.body.data, secret);
+    console.log(body);
+    if (body.username === email) {
+        if (body.password === pass) {
+            _jsonwebtoken2.default.sign({
+                "access_token": "jK06rHVB3o0KJMerdolD8eSi570MVyMCdefSNip4",
+                "token_type": "Bearer",
+                "expires_in": 86400,
+                "refresh_token": "GRq3UVgZh9wSENzTTlZ92eHeTzWMOAJL3HKKn6q4",
+                "user_id": "82"
+            }, secret, function (err, token) {
+                res.json(token);
+            });
+        } else {
+            res.status(500);
+            _jsonwebtoken2.default.sign({ "message": "wrong password" }, secret, function (err, token) {
+                res.json(token);
+            });
+        }
+    } else {
+        res.status(500);
+        _jsonwebtoken2.default.sign({ "message": "wrong username" }, secret, function (err, token) {
+            res.json(token);
+        });
+    }
+});
+
+//# sourceMappingURL=server-compiled.js.map
---
+++ server.js
@@ -0,0 +1,48 @@
+/**
+ * Created by pablos on 5/3/17.
+ */
+import express from 'express';
+import bodyParser   from 'body-parser';
+import jwt from 'jsonwebtoken';
+
+let secret = "SOMECOOLSECRET";
+
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+app.listen(3000, () => {
+    console.log('listening on 3000');
+});
+
+app.post("/auth", (req, res) =>{
+    const email = "email@email.com";
+    const pass = "pass";
+    const body = jwt.verify(req.body.data, secret);
+    console.log(body);
+    if(body.username === email){
+        if(body.password === pass){
+            jwt.sign({
+                "access_token": "jK06rHVB3o0KJMerdolD8eSi570MVyMCdefSNip4",
+                "token_type": "Bearer",
+                "expires_in": 86400,
+                "refresh_token": "GRq3UVgZh9wSENzTTlZ92eHeTzWMOAJL3HKKn6q4",
+                "user_id": "82"
+            }, secret, (err, token) =>{
+                res.json(token)
+            });
+        }else{
+            res.status(500);
+            jwt.sign({"message": "wrong password"}, secret, (err, token) =>{
+                res.json(token)
+            });
+        }
+    }else{
+       res.status(500);
+        jwt.sign({"message": "wrong username"}, secret, (err, token) =>{
+            res.json(token)
+        });
+    }
+});
