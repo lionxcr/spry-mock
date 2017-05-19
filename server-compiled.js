165701bece6e4943c0412bbd27f4481d2a669a8b
Pablo Segura <pablos@thehangar.cr>
2017-05-19 11:24:26 -0600

    Added brief

--- server-compiled.js
+++ server-compiled.js
@@ -82,7 +82,18 @@ app.post('/price', function (req, res) {
         var body = _jsonwebtoken2.default.verify(req.body.data, secret);
         var dealine = new Date(body.date);
         var diference = days_between(dealine, new Date());
-        var price = diference * 125.00;
+        var price = 0;
+        if (diference < 3) {
+            price = diference * 250.00;
+        } else if (diference < 6) {
+            price = diference * 200.00;
+        } else if (diference < 12) {
+            price = diference * 175.00;
+        } else if (diference < 24) {
+            price = diference * 150.00;
+        } else {
+            price = diference * 125.00;
+        }
         var tax = price * .28;
         var finalPrice = Math.round(price + tax);
         if (body.revision === false) {
@@ -116,5 +127,26 @@ function days_between(date1, date2) {
     // Convert back to days and return
     return Math.round(difference_ms / ONE_DAY + 1);
 }
+
+var newProductAnouncement = {
+    required: [{ id: 1, order: 0, title: "What’s the news?", description: "What are we announcing?" }, { id: 2, order: 1, title: "Differentiators/USPs", description: "What sets this apart from the rest(differentiators, unique selling points)?You can simply include links or attach documents." }, { id: 3, order: 2, title: "Key Features", description: "What are the important features? You can simply include links or attach documents." }, { id: 4, order: 3, title: "Target Customers", description: "Who should buy this?" }, { id: 5, order: 4, title: "Customer Benefits", description: "Why will it benefit them?" }],
+    type: "press_release_new_product",
+    optional: [{ id: 1, order: 0, title: "Three Key Messages", description: "What are the three key messages you’d like to highlight?" }, { id: 2, order: 1, title: "Relevant Trends", description: "What trends are relevant for this?" }, { id: 3, order: 2, title: "Availability", description: "Availability (Time / Pricing)" }, { id: 4, order: 3, title: "Customer Cases", description: "Can we cite any real customers or at least use cases?" }, { id: 5, order: 4, title: "Spokesperson", description: "Who should be quoted? (Name/Title)" }, { id: 6, order: 5, title: "Media Contact", description: "Media contact (name, phone number, email address)" }, { id: 7, order: 6, title: "Company Boilerplate", description: "" }, { id: 8, order: 7, title: "Distribution Date/City", description: "" }, { id: 9, order: 8, title: "Writing Style", description: "" }, { id: 10, order: 9, title: "Previous Releases", description: "Any previous releases that will help us?" }, { id: 11, order: 10, title: "Supporting Materials", description: "What about other materials and sources?" }, { id: 12, order: 11, title: "Additional Note", description: "What else should we know?" }]
+};
+
+app.get('/brief', function (req, res) {
+    if (req.headers.access_token === accessToken) {
+        _jsonwebtoken2.default.sign({
+            "brief": newProductAnouncement
+        }, secret, function (err, token) {
+            res.json(token);
+        });
+    } else {
+        res.status(401);
+        _jsonwebtoken2.default.sign({ "message": "unauthorized request" }, secret, function (err, token) {
+            res.json(token);
+        });
+    }
+});
 
 //# sourceMappingURL=server-compiled.js.map
