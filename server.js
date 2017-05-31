f4fddea2e4fd2afc74eb4912c924114d103ad465
Pablo Segura <pablos@thehangar.cr>
2017-05-31 14:36:01 -0600

    Updates to brief

--- server.js
+++ server.js
@@ -129,28 +129,28 @@ function days_between(date1, date2) {
 
 const newProductAnouncement = {
     required: [
-        {id:1, order: 0, title: "Order Title", description: "This is not a title of the draft"},
-        {id:2, order: 1, title: "What’s the news?", description: "What are we announcing?"},
-        {id:3, order: 2, title: "Differentiators/USPs", description: "What sets this apart from the rest(differentiators, unique selling points)?You can simply include links or attach documents."},
-        {id:4, order: 3, title: "Key Features", description: "What are the important features? You can simply include links or attach documents."},
-        {id:5, order: 4, title: "Target Customers", description: "Who should buy this?"},
-        {id:6, order: 5, title: "Customer Benefits", description: "Why will it benefit them?"},
-        {id:7, order: 6, title: "Related Industries", description: "What industries are related to this?"}
+        {id:1, order: 0, title: "Order Title", description: "This is not a title of the draft", sample: ""},
+        {id:2, order: 1, title: "What’s the news?", description: "What are we announcing?", sample: "CL Elec. launches the new CL ColorBook laptop series"},
+        {id:3, order: 2, title: "Differentiators/USPs", description: "What sets this apart from the rest (differentiators, unique selling points)? You can simply include links or attach documents.", sample: "It comes with seven customizable covers so users can change the covers based on their mood and TPO"},
+        {id:4, order: 3, title: "Key Features", description: "What are the important features? You can simply include links or attach documents.", sample: ""},
+        {id:5, order: 4, title: "Target Customers", description: "Who should buy this?", sample: "Millennial, female, style-conscious users"},
+        {id:6, order: 5, title: "Customer Benefits", description: "Why will it benefit them?", sample: "It’s fun and stylish to carry, they can match the cover with OOTD"},
+        {id:7, order: 6, title: "Related Industries", description: "What industries are related to this?", sample: ""}
     ],
     type: "press_release_new_product",
     optional: [
-        {id:1, order: 0, title: "Three Key Messages", description: "What are the three key messages you’d like to highlight?"},
-        {id:2, order: 1, title: "Relevant Trends", description: "What trends are relevant for this?"},
-        {id:3, order: 2, title: "Availability", description: "Availability (Time / Pricing)"},
-        {id:4, order: 3, title: "Customer Cases", description: "Can we cite any real customers or at least use cases?"},
-        {id:5, order: 4, title: "Spokesperson", description: "Who should be quoted? (Name/Title)"},
-        {id:6, order: 5, title: "Media Contact", description: "Media contact (name, phone number, email address)"},
-        {id:7, order: 6, title: "Company Boilerplate", description: ""},
-        {id:8, order: 7, title: "Distribution Date/City", description: ""},
-        {id:9, order: 8, title: "Writing Style", description: ""},
-        {id:10, order:9, title: "Previous Releases", description: "Any previous releases that will help us?"},
-        {id:11, order:10, title: "Supporting Materials", description: "What about other materials and sources?"},
-        {id:12, order:11, title: "Additional Note", description: "What else should we know?"}
+        {id:1, order: 0, title: "Three Key Messages", description: "What are the three key messages you’d like to highlight?", sample: ""},
+        {id:2, order: 1, title: "Relevant Trends", description: "What trends are relevant for this?", sample: "Gadget-as-a-fashion accessory trend"},
+        {id:3, order: 2, title: "Availability", description: "Availability (Time / Pricing)", sample: "Jan 2017/ starting at USD 380"},
+        {id:4, order: 3, title: "Customer Cases", description: "Can we cite any real customers or at least use cases?", sample: ""},
+        {id:5, order: 4, title: "Spokesperson", description: "Who should be quoted? (Name/Title)", sample: ""},
+        {id:6, order: 5, title: "Media Contact", description: "Media contact (name, phone number, email address)", sample: ""},
+        {id:7, order: 6, title: "Company Boilerplate", description: "", sample: ""},
+        {id:8, order: 7, title: "Distribution Date/City", description: "", sample: ""},
+        {id:9, order: 8, title: "Writing Style", description: "", sample: ""},
+        {id:10, order:9, title: "Previous Releases", description: "Any previous releases that will help us?", sample: ""},
+        {id:11, order:10, title: "Supporting Materials", description: "What about other materials and sources?", sample: ""},
+        {id:12, order:11, title: "Additional Note", description: "What else should we know?", sample: "We want to focus more on the design and affordability, and less on product performance which is still competitive among the entry level laptops but not outstanding. \nWe want to position ColorBook as a fashion & lifestyle product, targeting fashion-conscious millennial users who use a laptop for content consumption, entertainment, social networking – not necessarily for heavy content creation"}
     ]
 };
 
