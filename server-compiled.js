3b8f5ae1aa0b1e3f343ceb92ff4b79ed0d541734
Pablo Segura <pablos@thehangar.cr>
2017-06-05 17:11:37 -0600

    Date fixes

--- server-compiled.js
+++ server-compiled.js
@@ -164,10 +164,10 @@ var orderStatus = function orderStatus(name, status, action, description, addTim
 
 var getUTCTimeStamp = function getUTCTimeStamp(minutes, addHour) {
     var now = new Date();
-    now.setHours(now.getHours() + 1);
     if (addHour) {
-        now.setMinutes(minutes);
+        now.setHours(now.getHours() + 1);
     }
+    now.setMinutes(minutes);
     var date = Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds(), now.getUTCMilliseconds());
     return new Date(date).toISOString();
 };
--- server.js
+++ server.js
@@ -184,10 +184,10 @@ class orderStatus{
 
 const getUTCTimeStamp = (minutes, addHour) => {
     let now = new Date();
-    now.setHours(now.getHours()+1);
     if(addHour){
-        now.setMinutes(minutes);
+        now.setHours(now.getHours()+1);
     }
+    now.setMinutes(minutes);
     const date = Date.UTC(now.getUTCFullYear(),now.getUTCMonth(), now.getUTCDate() ,
         now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds(), now.getUTCMilliseconds());
     return new Date(date).toISOString();
