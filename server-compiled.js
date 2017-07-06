43309bce27ffcbbaba9da56d8ad0b2f22e9d8abb
Pablo Segura <pablos@thehangar.cr>
2017-07-06 17:36:14 -0600

    Changes

--- server-compiled.js
+++ server-compiled.js
@@ -181,13 +181,13 @@ var getUTCTimeStamp = function getUTCTimeStamp(minutes) {
 var OrderHistory = function OrderHistory() {
     _classCallCheck(this, OrderHistory);
 
-    this.orderHistory = [new orderStatus("Quick quality check", "in_progress", true, "We did our standard quality check.", 1, "See what we think"), new orderStatus("Provide Feedback", "pending", true, "Review the work", 20, "Feedback submitted "), new orderStatus("Working on Feedback", "pending", false, "We are making some changes", 25, "Feedback developed"), new orderStatus("Deliver Order", "pending", false, "", 30, "Order delivered")];
+    this.orderHistory = [new orderStatus("Working in Order", "in_progress", false, "Currently we are working on your request, we carefully review the brief, do research and then start writing.", 1, ""), new orderStatus("Provide Feedback", "pending", true, "Review the work", 20, "Feedback submitted "), new orderStatus("Working on Feedback", "pending", false, "We are making some changes", 25, "Feedback developed"), new orderStatus("Deliver Order", "pending", false, "", 30, "Order delivered")];
 };
 
 var FreelancerOrder = function FreelancerOrder() {
     _classCallCheck(this, FreelancerOrder);
 
-    this.orderHistory = [new orderStatus("Start Writing", "completed", false, "Currently we are working on your request, we carefully review the brief, do research and then start writing.", 45, "Order developed"), new orderStatus("Client Review", "in_progress", true, "Review the work", 85, "Feedback submitted"), new orderStatus("Work on Feedback", "pending", false, "We are making some changes", 145, "Feedback developed"), new orderStatus("Finish Order", "pending", false, "", 205, "Order delivered")];
+    this.orderHistory = [new orderStatus("Quick quality check", "in_progress", true, "We did our standard quality check.", 1, "See what we think"), new orderStatus("Client Review", "pending", true, "Review the work", 85, "Feedback submitted"), new orderStatus("Work on Feedback", "pending", false, "We are making some changes", 145, "Feedback developed"), new orderStatus("Finish Order", "pending", false, "", 205, "Order delivered")];
 };
 
 app.get('/order/status', function (req, res) {
--- server.js
+++ server.js
@@ -194,7 +194,7 @@ const getUTCTimeStamp = (minutes) => {
 class OrderHistory{
     constructor(){
         this.orderHistory = [
-            new orderStatus("Quick quality check", "in_progress", true, "We did our standard quality check.",1,"See what we think"),
+            new orderStatus("Working in Order", "in_progress", false, "Currently we are working on your request, we carefully review the brief, do research and then start writing.",1,""),
             new orderStatus("Provide Feedback", "pending", true, "Review the work",20, "Feedback submitted "),
             new orderStatus("Working on Feedback", "pending", false, "We are making some changes",25,"Feedback developed"),
             new orderStatus("Deliver Order","pending", false, "",30,"Order delivered")
@@ -205,8 +205,8 @@ class OrderHistory{
 class FreelancerOrder{
     constructor(){
         this.orderHistory = [
-            new orderStatus("Start Writing", "completed", false, "Currently we are working on your request, we carefully review the brief, do research and then start writing.",45,"Order developed"),
-            new orderStatus("Client Review", "in_progress", true, "Review the work",85, "Feedback submitted"),
+            new orderStatus("Quick quality check", "in_progress", true, "We did our standard quality check.",1,"See what we think"),
+            new orderStatus("Client Review", "pending", true, "Review the work",85, "Feedback submitted"),
             new orderStatus("Work on Feedback", "pending", false, "We are making some changes",145,"Feedback developed"),
             new orderStatus("Finish Order","pending", false, "",205,"Order delivered")
         ];
